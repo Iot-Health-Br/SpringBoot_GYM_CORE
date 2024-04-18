b7defd2a33cce23c087550e4d32b2d8c1602a06b
Iot-Health-Br <igl_macedo@hotmail.com>
2024-04-18 19:39:06 -0300

    V1.1.7 - Estruturada a tela minha matricula, no perfil do usuário! Código testado !

--- src/main/java/com/cadastro/usuario/Controller/LoginController.java
+++ src/main/java/com/cadastro/usuario/Controller/LoginController.java
@@ -1,40 +1,61 @@
 package com.cadastro.usuario.Controller;
 
+import com.cadastro.usuario.Model.Adm;
+import com.cadastro.usuario.Model.LoginUser;
+import com.cadastro.usuario.Model.Usuario;
 import com.cadastro.usuario.Repository.AdmRepository;
 import com.cadastro.usuario.Repository.UsuarioRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
-
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 public class LoginController {
-
     @Autowired
     private UsuarioRepository userRepository;
-
     @Autowired
     private AdmRepository admRepository;
 
-    // Método para exibir a página de login
     @GetMapping("/login")
     public String loginForm() {
         return "login";
     }
 
-    // Método para processar o login
+    // Inclua isso para limpar o atributo quando necessário
+    @GetMapping("/logout")
+    public String logout(SessionStatus status) {
+        status.setComplete();
+        return "redirect:/login";
+    }
+
     @PostMapping("/login")
-    public ModelAndView processLogin(@RequestParam("cpf") String cpf, @RequestParam("senha") String senha) {
-        if (admRepository.findByCpfAndSenha(cpf, senha).isPresent()) {
+    public ModelAndView processLogin(@RequestParam("cpf") String cpf, @RequestParam("senha") String senha, HttpSession session) {
+        Optional<Adm> adm = admRepository.findByCpfAndSenha(cpf, senha);
+        Optional<Usuario> user = userRepository.findByCpfAndSenha(cpf, senha);
+
+        if (adm.isPresent()) {
+            Adm admin = adm.get();
+            LoginUser loginUser = new LoginUser();
+            loginUser.setNome(admin.getNome());
+            loginUser.setTelefone(admin.getTelefone());
+            session.setAttribute("user", loginUser);
             return new ModelAndView("redirect:/homeAdm");
         }
-        else if (userRepository.findByCpfAndSenha(cpf, senha).isPresent()) {
+        else if (user.isPresent()) {
+            Usuario usuario = user.get();
+            LoginUser loginUser = new LoginUser();
+            loginUser.setNome(usuario.getNome());
+            loginUser.setTelefone(usuario.getTelefone());
+            session.setAttribute("user", loginUser);
             return new ModelAndView("redirect:/homeUser");
         }
-
         else {
             ModelAndView modelAndView = new ModelAndView("login");
             modelAndView.addObject("error", "Usuário não encontrado");
@@ -42,4 +63,3 @@ public class LoginController {
         }
     }
 }
-
--- src/main/java/com/cadastro/usuario/Controller/UserController.java
+++ src/main/java/com/cadastro/usuario/Controller/UserController.java
@@ -1,7 +1,11 @@
 package com.cadastro.usuario.Controller;
 
+import com.cadastro.usuario.Model.LoginUser;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -9,5 +13,15 @@ public class UserController {
     @GetMapping("/homeUser")
     public String home() {
         return "homeUser"; // Retorna o nome do arquivo homeAdm.html que está em src/main/resources/templates
+    }
+
+    /////////////////////////Tela Minha Matricula///////////////////////////////////
+    @GetMapping("/matriculaUser")
+    public ModelAndView showMatriculaUser(HttpSession session) {
+        LoginUser user = (LoginUser) session.getAttribute("user");
+        ModelAndView modelAndView = new ModelAndView("matriculaUser");
+        modelAndView.addObject("nome", user.getNome());
+        modelAndView.addObject("telefone", user.getTelefone());
+        return modelAndView;
     }
 }
